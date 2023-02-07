e9bb90845df6a2d231788c17b6c9aa9176c67a27
전해림 <101709981+HAERIM0@users.noreply.github.com>
2023-02-07 11:32:03 +0900

    휴 ~

--- daeitso/src/App.js
+++ daeitso/src/App.js
@@ -1,19 +1,15 @@
-
-import "./App.css";
-import Header from "./components/Header/Header";
-
-function App() {
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Startpage from "./page/Startpage/Startpage";
+import Header from "./components/Header/Header";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Startpage />} />
+        <Route path="/main" element={<Header />} />
       </Routes>
     </BrowserRouter>
   );
-
 }
 
 export default App;
