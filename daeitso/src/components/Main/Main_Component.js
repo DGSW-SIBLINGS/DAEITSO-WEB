817d45e818813e46417007768b40daa1946590d9
kimyerim01118 <72791856+yr0118kim@users.noreply.github.com>
2023-02-07 18:46:54 +0900

    push

--- daeitso/src/components/Main/Main_Component.js
+++ daeitso/src/components/Main/Main_Component.js
@@ -6,13 +6,44 @@ width:180px;
 height:260px;
 margin-right: 10px;
 background-color: #f2f2f2;
+
+`
+
+const ComponentTitle = styled.div`
+font-family: pretendard;
+font-size:1rem;
+color:black;
+font-weight: 600;
+margin:5px;
+
+`
+const ComponentContent = styled.div`
+font-family: pretendard;
+font-size:0.8rem;
+color:black;
+font-weight: 500;
+margin:5px;
+
+`
+const ComponentTag = styled.div`
+font-family: pretendard;
+font-size:0.8rem;
+color:#3D8AF5;
+font-weight: 500;
+margin:5px;
+margin-top:20px;
 `
 function Component(props){
     return(
         <>{
-            console.log(props.post.content)
+            console.log(props.post.imgUrls[0].imgUrl)
         }
-        <ComponentWrap>d</ComponentWrap>
+        <ComponentWrap>
+            <img alt = "none" src={props.post.imgUrls[0].imgUrl} style={{width:180,height:180}}></img>
+            <ComponentTitle>{props.post.title}</ComponentTitle>
+            <ComponentContent>{props.post.price}원</ComponentContent>
+            <ComponentTag>#{props.post.category}</ComponentTag>
+        </ComponentWrap>
         </>
     )
 }
