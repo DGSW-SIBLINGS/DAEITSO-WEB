caad547ddccc9a8d54d5fe76099b034c033a9470
전해림 <101709981+HAERIM0@users.noreply.github.com>
2023-02-07 16:12:05 +0900

    imgfileupload

--- daeitso/src/components/File/file.style.js
+++ daeitso/src/components/File/file.style.js
@@ -1,6 +1,42 @@
 import styled from "styled-components";
 
 export const FileBox = styled.div``;
-export const FileList = styled.div``;
-export const FileH1 = styled.div``;
-export const FileInputBox = styled.div``;
+export const FileList = styled.div`
+  margin-left: 20%;
+  margin-top: 25px;
+  font-size: 12px;
+`;
+export const FileH1 = styled.div`
+  h2 {
+    margin-left: -24px;
+  }
+`;
+export const FileInputBox = styled.div`
+  h3 {
+    margin-top: 20px;
+  }
+`;
+
+export const FileLi = styled.div`
+  /* margin-top: 5px; */
+  margin-bottom: 10px;
+  color: #858585;
+`;
+export const FileImgBt = styled.div`
+  display: flex;
+
+  button {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 150px;
+    height: 150px;
+    font-size: 11px;
+    border: 0;
+  }
+  .Camera-icon {
+    font-size: 25px;
+    color: #636363;
+  }
+`;
