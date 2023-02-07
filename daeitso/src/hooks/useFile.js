160a641a362134bf6b8f947b1e0ad5c9dc139848
전해림 <101709981+HAERIM0@users.noreply.github.com>
2023-02-08 02:38:07 +0900

    push

--- daeitso/src/hooks/useFile.js
+++ daeitso/src/hooks/useFile.js
@@ -1,29 +1,10 @@
-import React from "react";
-import { customAxios } from "../lib/axios/customAxios";
-import { postFileUrlAtom } from "../recoil/uploadAtom";
-import { useRecoilState } from "recoil";
+// import React from "react";
+// import { customAxios } from "../lib/axios/customAxios";
+// import { postFileUrlAtom } from "../recoil/uploadAtom";
+// import { useRecoilState } from "recoil";
 
-export const useFile = () => {
-  const [fileUrl, setFileUrl] = useRecoilState(postFileUrlAtom);
-  const uploadImg = async (e) => {
-    const files = e.target.files;
-    const formData = new FormData();
+// export const useFile = ({ file }) => {
+//   const [fileUrl, setFileUrl] = useRecoilState(postFileUrlAtom);
 
-    for (let i = 0; i < files.length; i++) {
-      formData.append("file", files[i]);
-    }
-
-    try {
-      const { data } = await customAxios.post("/file", formData);
-      const arr = [];
-
-      data.data.forEach((value) => {
-        arr.push(value);
-      });
-      setFileUrl(arr);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  return { uploadImg };
-};
+//   return { uploadImg };
+// };
--- daeitso/src/recoil/uploadAtom.js
+++ daeitso/src/recoil/uploadAtom.js
@@ -4,3 +4,33 @@ export const postFileUrlAtom = atom({
   key: "postFileUrlAtom",
   default: [],
 });
+
+export const postUploadnameAtom = atom({
+  key: "postUploadnameAtom",
+  default: "",
+});
+
+export const postUploadpriceAtom = atom({
+  key: "postUploadpriceAtom",
+  default: "",
+});
+
+export const postUploadfreeAtom = atom({
+  key: "postUploadfreeAtom",
+  default: "",
+});
+
+export const postUploadinfoAtom = atom({
+  key: "postUploadinfoAtom",
+  default: "",
+});
+
+export const postUploadtagAtom = atom({
+  key: "postUploadtagAtom",
+  default: "",
+});
+
+export const postUploadlocationAtom = atom({
+  key: "postUploadlocationAtom",
+  default: "",
+});
