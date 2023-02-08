dde158278666bcf5a55af8a7e5cc0a4dcf6bf7d0
전해림 <101709981+HAERIM0@users.noreply.github.com>
2023-02-08 12:06:39 +0900

    push

--- daeitso/src/hooks/useMyfile.js
+++ daeitso/src/hooks/useMyfile.js
@@ -1,17 +1,14 @@
 export const useMyfile = () => {
-    const handleChangeFile = async (e) => {
-        const files = e.target.files;
-        const formData = new FormData();
+  const handleChangeFile = async (e) => {
+    const files = e.target.files;
+    const formData = new FormData();
 
-        for (let i = 0; i < files.length === 1; i++){
-            formData.append("file",files[i]);
-        }
-        try{
+    for (let i = 0; i < files.length === 10; i++) {
+      formData.append("file", files[i]);
+    }
+    try {
+    } catch (e) {}
+  };
 
-        }catch(e){
-            
-        }
-    };
-    
-    return{handleChangeFile};
-}
+  return { handleChangeFile };
+};
